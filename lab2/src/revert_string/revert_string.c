9ab84674568e833e7cd9f2fc15c556214099a624
sokoltsov <direct0vision@gmail.com>
2019-03-12 15:24:37 +0000

    Выполнил 1 и 2 задания

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,17 @@
 #include "revert_string.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+    int len = strlen(str);
+    char *revstr = malloc(sizeof(char) * len);
+    for(int i = 0; i < len; i++)
+    {
+        revstr[i] = str[len-i-1];
+    }
+    strcpy(str, revstr);
+    free(revstr);
 }
 
--- lab2/src/swap/swap.c
+++ lab2/src/swap/swap.c
@@ -2,5 +2,8 @@
 
 void Swap(char *left, char *right)
 {
-	// ваш код здесь
+	char buf;
+	buf = *left;
+	*left = *right;
+    *right = buf;
 }
